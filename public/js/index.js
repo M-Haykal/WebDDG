ee9e0cc0f5a9e4ebb1dfec679a87d0cf536f7fcc
M. Haykal <haykalmuhammad456@gmail.com>
2024-04-03 21:12:33 +0700

    new look

--- public/js/index.js
+++ public/js/index.js
@@ -84,3 +84,18 @@
     });
   });
 })();
+
+function sendMessage() {
+  var name = document.getElementById("name").value;
+  var message = document.getElementById("message").value;
+  var whatsapp = "6289509758465";
+
+  var url =
+    "https://api.whatsapp.com/send?phone=" +
+    whatsapp +
+    "&text=Halo, nama saya " +
+    name +
+    ". " +
+    message;
+  window.open(url);
+}
